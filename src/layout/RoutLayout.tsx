cf289dd3c0a143ffa3569119954c58663478b263
liting <liting_qiu@hotmail.com>
2023-03-06 14:57:31 +0100

    add RoutLayout component

---
+++ src/layout/RoutLayout.tsx
@@ -0,0 +1,14 @@
+
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/footer/Footer'
+import Header from '../components/header/Header'
+
+export default function RoutLayout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
